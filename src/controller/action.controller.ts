bfbbc92cc64ddfd7c1c2fc5bdf775e83d40f547d
Florent Fortat <florent@fortat.fr>
2020-03-31 00:20:04 +0200

    Mock base controller functions

--- src/controller/action.controller.ts
+++ src/controller/action.controller.ts
@@ -1,3 +1,23 @@
-export class Action {
+export interface Action {
+    id: number;
+    timestamp: number;
+}
 
+export class ActionController {
+    static listActions(): Action[] {
+        // TODO list from DB
+        return [
+            {id: 0, timestamp: new Date().getTime()}
+        ];
+    }
+
+    static addAction(timestamp: number): Action {
+        // TODO save in DB
+        return {id: 0, timestamp};
+    }
+
+    static getAction(id: number): Action {
+        // TODO get from DB
+        return {id, timestamp: new Date().getTime()};
+    }
 }
--- src/route/action.ts
+++ src/route/action.ts
@@ -1,7 +1,16 @@
 import * as express from 'express';
+import {ActionController} from "../controller/action.controller";
 
 export function register(app: express.Application, baseUrl: string): void {
-    app.get(baseUrl + '/action', (req, res) => {
-        res.json({hello: 'world'});
+    app.get(baseUrl + '/action', (request, result) => {
+        result.json(ActionController.listActions());
+    });
+
+    app.get(baseUrl + '/action/:actionId', (request, result) => {
+        result.json(ActionController.getAction(+request.params.actionId));
+    });
+
+    app.post(baseUrl + '/action', (request, result) => {
+        result.json(ActionController.addAction(new Date().getTime()));
     });
 }
