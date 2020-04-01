636acbd5c8816870b8e5216ca723faca014c83fb
Florent Fortat <florent@fortat.fr>
2020-04-01 10:51:06 +0200

    Add mongoose connection and parser

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
-import express from "express";
-import * as routes from "./route";
+import express from 'express';
+import * as routes from './route';
+import mongoose from 'mongoose';
 
 const app = express();
 const port = 8081; // default port to listen
 
+mongoose.connect('mongodb://localhost/tracking', {useNewUrlParser: true, useUnifiedTopology: true});
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 routes.register(app);
 
 // start the Express server
@@ -11,3 +16,7 @@ app.listen( port, () => {
     // tslint:disable-next-line:no-console
     console.log( `server started at http://localhost:${ port }` );
 } );
+
+process.on('exit', () => {
+    mongoose.connection.close();
+})
