5c680cd46bd927d8c1dba36f92c63eac9d4f40c0
Florent Fortat <florent@fortat.fr>
2020-04-02 10:32:04 +0200

    Implement product endpoint

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -1,3 +1,48 @@
-export class Product {
+import {Product, IProduct} from '../model/product.model';
 
+export class ProductController {
+    static listProducts(filter = {}): Promise<any[]> {
+        return new Promise((resolve, reject) => {
+            Product.find(filter, (error, result) => {
+                if (error) {
+                    reject(error);
+                    return;
+                }
+
+                resolve(result);
+            });
+        })
+    }
+
+    static async addProduct(task: IProduct): Promise<any> {
+        return new Promise((resolve, reject) => {
+            const newProduct = new Product(task);
+
+            Product.find(task, (error, result) => {
+                if (error) {
+                    reject(error);
+                    return;
+                }
+
+                if (result.length === 0) {
+                    newProduct.save().then((savedProduct) => resolve(savedProduct));
+                } else {
+                    resolve(result[0]);
+                }
+            });
+        });
+    }
+
+    static getProduct(id: string): Promise<any> {
+        return new Promise((resolve, reject) => {
+            Product.findById(id, (error, result) => {
+                if (error) {
+                    reject(error);
+                    return;
+                }
+
+                resolve(result);
+            })
+        });
+    }
 }
--- src/route/product.ts
+++ src/route/product.ts
@@ -1,7 +1,22 @@
 import * as express from 'express';
+import {ProductController} from "../controller/product.controller";
 
 export function register(app: express.Application, baseUrl: string): void {
-    app.get(baseUrl + '/product', (req, res) => {
-        res.json({hello: 'world'});
+    app.get(baseUrl + '/product', (request, response) => {
+        ProductController.listProducts(request.query).then((products) => {
+            response.json(products);
+        });
+    });
+
+    app.get(baseUrl + '/product/:productId', (request, response) => {
+        ProductController.getProduct(request.params.productId).then((product) => {
+            response.json(product);
+        });
+    });
+
+    app.post(baseUrl + '/product', (request, response) => {
+        ProductController.addProduct(request.body).then((product) => {
+            response.json(product);
+        })
     });
 }
