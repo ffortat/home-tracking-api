a052950436ac85c22481187891d7fcc6ee88376e
Florent Fortat <florent@fortat.fr>
2020-04-18 14:47:23 +0200

    Add API routes

--- src/route/api/index.ts
+++ src/route/api/index.ts
@@ -1,12 +1,16 @@
 import * as express from "express";
-import * as task from './task';
 import * as action from './action';
+import * as category from './category';
+import * as place from './place';
 import * as product from './product';
+import * as task from './task';
 
 const baseAPIRoute = '/api';
 
 export function register(app: express.Application): void {
     action.register(app, baseAPIRoute);
+    category.register(app, baseAPIRoute);
+    place.register(app, baseAPIRoute);
     product.register(app, baseAPIRoute);
     task.register(app, baseAPIRoute);
 }
