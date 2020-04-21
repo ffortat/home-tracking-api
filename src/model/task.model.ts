c0d29b6f6f30ebf2979960a4de00831626cf3d09
Florent Fortat <florent@fortat.fr>
2020-04-22 00:37:35 +0200

    Complete task model

--- src/model/task.model.ts
+++ src/model/task.model.ts
@@ -3,17 +3,35 @@ import mongoose, {Schema} from "mongoose";
 export interface ITask {
     _id?: string;
     name: string;
+    start: Date;
     times: number;
-    recurrence: string;
+    frequency: string;
+    interval: number;
+    byDay: number[];
 }
 
 const TaskSchema = new Schema({
     name: String,
-    times: Number,
-    recurrence: {
+    start: {
+        type: Date,
+        default: Date.now
+    },
+    times: {
+        type: Number,
+        default: 1
+    },
+    frequency: {
         type: String,
         enum: ['hourly', 'daily', 'weekly', 'monthly', 'yearly'],
         default: 'weekly'
+    },
+    interval: {
+        type: Number,
+        default: 1
+    },
+    byDay: {
+        type: [Number],
+        default: undefined
     }
 });
 
